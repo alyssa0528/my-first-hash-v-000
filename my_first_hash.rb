58727f6cbf1815b26d908ab97174f18855025d6a
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-07 22:05:55 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {
-    "name" => "Alyssa Kim"
+    "name" => "Alyssa Kim",
     "city" => "Dublin"
-}
+    }
 end
 
 
@@ -12,9 +12,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2 
-    "oil paintings" => 3
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2, 
+    "oil paintings" => 3,
   }
 end
 
